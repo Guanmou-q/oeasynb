6957521b782f1c403143317d149fca690f58692f
guan <2306883407@qq.com>
2021-01-22 16:11:18 +0800

    gh-pages

--- 001Basic/js/js.js
+++ 001Basic/js/js.js
@@ -1,10 +1,6 @@
 function alert1(){
-
-    alert("1111");
-    alert("2222");
-    alert("3333");
+    alert("你好杜艺林");
+    alert("请欣赏唐诗");
+   
 }
-alert("nihao")
 alert1();
-alert1();
-alert("你喜欢我吗")
